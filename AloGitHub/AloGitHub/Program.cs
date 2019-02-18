5ae1d79942f2e98a1d8c2d4da9fd3260feb6062b
Lucas Ferreira Barros <lucasferreira_b@live.com>
2019-02-17 23:01:28 -0300

    Primeira atualização da minha Solução

---
+++ AloGitHub/AloGitHub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AloGitHub
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			NewMethod();
+		}
+
+		private static void NewMethod()
+		{
+			Console.WriteLine("Primeiro Teste com GitHub!");
+			Console.ReadKey();
+		}
+	}
+}
